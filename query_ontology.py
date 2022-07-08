05caa2d91deb511ab1a25b8909d5900b17aa05ce
tbohne <tim.bohne@dfki.de>
2022-07-08 15:31:28 +0200

    add condition query

--- query_ontology.py
+++ query_ontology.py
@@ -9,23 +9,42 @@ def complete_ontology_entry(entry):
 
 
 def query_fault_causes_by_dtc(dtc, g):
-    print("QUERY: fault causes for " + dtc)
+    print("####################################")
+    print("QUERY: fault causes for", dtc)
+    print("####################################")
     dtc_entry = complete_ontology_entry(dtc)
     represents_entry = complete_ontology_entry('represents')
     fault_cause_entry = complete_ontology_entry('FaultCause')
     has_cause_entry = complete_ontology_entry('hasCause')
     s = f"""
-    SELECT ?cause WHERE {{
-        {dtc_entry} {represents_entry} ?condition .
-        ?cause a {fault_cause_entry} .
-        ?condition {has_cause_entry} ?cause .
-    }}
-    """
+        SELECT ?cause WHERE {{
+            {dtc_entry} {represents_entry} ?condition .
+            ?cause a {fault_cause_entry} .
+            ?condition {has_cause_entry} ?cause .
+        }}
+        """
+    for row in g.query(s):
+        print("--> ", str(row).split(ONTOLOGY_PREFIX.replace("<", "").replace(">", ""))[1].replace("'),)", ""))
+
+
+def query_fault_condition_by_dtc(dtc, g):
+    print("####################################")
+    print("QUERY: fault condition for", dtc)
+    print("####################################")
+    dtc_entry = complete_ontology_entry(dtc)
+    represents_entry = complete_ontology_entry('represents')
+    s = f"""
+        SELECT ?condition WHERE {{
+            {dtc_entry} {represents_entry} ?condition .
+        }}
+        """
     for row in g.query(s):
         print("--> ", str(row).split(ONTOLOGY_PREFIX.replace("<", "").replace(">", ""))[1].replace("'),)", ""))
 
 
 if __name__ == '__main__':
     graph = rdflib.Graph()
+    DTC = "P0138"
     graph = graph.parse(ONTOLOGY_FILE, format='xml')
-    query_fault_causes_by_dtc("P0138", graph)
+    query_fault_causes_by_dtc(DTC, graph)
+    query_fault_condition_by_dtc(DTC, graph)
