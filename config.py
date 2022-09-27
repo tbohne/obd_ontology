3c0fe165085177adf61620dca46e0930a91c724a
tbohne <tim.bohne@dfki.de>
2022-09-27 15:39:25 +0200

    refine connection controller / add doc

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 KNOWLEDGE_GRAPH_FILE = "obd_knowledge_graph.owl"
 ONTOLOGY_PREFIX = "http://www.semanticweb.org/diag_ontology#"
 FUSEKI_URL = "http://127.0.0.1:3030"
+SPARQL_ENDPOINT = "/OBD/sparql"
+DATA_ENDPOINT = "/OBD/data"
--- connection_controller.py
+++ connection_controller.py
@@ -7,28 +7,44 @@ import re
 import requests
 from rdflib import Namespace, RDF, Literal, Graph, URIRef
 
-from config import ONTOLOGY_PREFIX, FUSEKI_URL
+from config import ONTOLOGY_PREFIX, FUSEKI_URL, SPARQL_ENDPOINT, DATA_ENDPOINT
 from fact import Fact
 
 
 class ConnectionController:
+    """
+    Establishes the connection to the knowledge graph hosted by the Apache Jena Fuseki server.
+    Performs queries as well as knowledge graph extensions via HTTP requests.
+    """
 
-    def __init__(self, namespace, fuseki_url=FUSEKI_URL) -> None:
+    def __init__(self, namespace: str, fuseki_url=FUSEKI_URL) -> None:
         self.namespace = Namespace(namespace)
         self.fuseki_url = fuseki_url
         self.graph = Graph()
         self.graph.bind("", self.namespace)
 
-    def query_knowledge_graph(self, query: str):
+    def query_knowledge_graph(self, query: str) -> list:
+        """
+        Sends an HTTP request containing the specified query to the knowledge graph server.
+
+        :param query: query to be sent to knowledge graph server
+        :return: query results
+        """
         print("query knowledge graph..")
         print(query)
-        res = requests.post(self.fuseki_url + "/OBD/sparql", query,
+        res = requests.post(self.fuseki_url + SPARQL_ENDPOINT, query,
                             headers={'Content-Type': 'application/sparql-query', 'Accept': 'application/json'})
         if res.status_code != 200:
             print("HTTP status code:", res.status_code)
+        print(type(res.json()["results"]["bindings"]))
         return res.json()["results"]["bindings"]
 
     def extend_knowledge_graph(self, facts: list) -> None:
+        """
+        Sends an HTTP request containing the facts to be entered into the knowledge graph to the knowledge graph server.
+
+        :param facts: facts to be entered into the knowledge graph
+        """
         print("extend knowledge graph..")
         graph = Graph()
         for fact in facts:
@@ -38,12 +54,18 @@ class ConnectionController:
             else:
                 graph.add((self.get_uri(fact.triple[0]), self.get_uri(fact.triple[1]), self.get_uri(fact.triple[2])))
 
-        res = requests.post(self.fuseki_url + "/OBD/data", data=graph.serialize(format="ttl"),
+        res = requests.post(self.fuseki_url + DATA_ENDPOINT, data=graph.serialize(format="ttl"),
                             headers={'Content-Type': 'text/turtle'})
         if res.status_code != 200:
             print("HTTP status code:", res.status_code)
 
     def get_uri(self, triple_ele: str) -> URIRef:
+        """
+        Returns the specified triple element as feasible URI reference.
+
+        :param triple_ele: triple element to get URI reference for
+        :return: URI reference for triple element
+        """
         if re.match("(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])", triple_ele):
             return URIRef(triple_ele)
         else:
@@ -53,22 +75,13 @@ class ConnectionController:
 if __name__ == '__main__':
     connection = ConnectionController(ONTOLOGY_PREFIX)
 
-    # q = """
-    #     SELECT ?s ?p ?o
-    #     WHERE {
-    #         ?s ?p ?o
-    #     }
-    #     LIMIT 25
-    #     """
-    # response = connection.query_knowledge_graph(q)
-    # print("number of res:", len(response))
-    # print("RES:")
-    # print(response)
+    # query example
+    q = "SELECT ?s ?p ?o WHERE { ?s ?p ?o } LIMIT 25"
+    response = connection.query_knowledge_graph(q)
+    print(response)
 
-    ########################################################
-
+    # extension example
     onto_namespace = Namespace(ONTOLOGY_PREFIX)
-
     fact_list = [
         Fact(('car_1', RDF.type, onto_namespace["Vehicle"].toPython())),
         Fact(("car_1", onto_namespace.model, 'Mazda3'), property_fact=True),
@@ -77,5 +90,4 @@ if __name__ == '__main__':
         Fact(("car_1", onto_namespace.VIN, '1234567890ABCDEFGHJKLMNPRSTUVWXYZ'), property_fact=True),
         Fact(("OWLNamedIndividual_181b81a8_3e76_4ab8_bee8_33d7508ac04a", onto_namespace.occurredIn, 'car_1'))
     ]
-
     connection.extend_knowledge_graph(fact_list)
