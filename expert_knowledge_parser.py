7252e2d663d0c1718c0ee30ba1bca12fe66f6ea8
tbohne <tim.bohne@dfki.de>
2022-10-04 09:15:56 +0200

    prepare expert knowledge parser for updated ontology

--- expert_knowledge_parser.py
+++ expert_knowledge_parser.py
@@ -4,7 +4,8 @@
 
 class ExpertKnowledgeParser:
     """
-    Parses OBD-related expert knowledge from template file (cf. `templates/expert_knowledge_template.txt`).
+    Parses OBD-related expert knowledge from template file (cf. `templates/dtc_expert_template.txt`,
+    `templates/component_expert_template`, `templates/subsystem_expert_template.txt`).
     """
 
     def __init__(self, knowledge_file: str) -> None:
@@ -15,12 +16,29 @@ class ExpertKnowledgeParser:
         self.suspect_components = []
         self.occurs_with = []
 
+    def parse_dtc_expert_template(self, lines: list) -> None:
+        pass
+
+    def parse_component_expert_template(self, lines: list) -> None:
+        pass
+
+    def parse_subsystem_expert_template(self, lines: list) -> None:
+        pass
+
     def parse_knowledge(self) -> None:
         """
         Parses the OBD-related expert knowledge.
         """
         with open(self.knowledge_file) as f:
             lines = f.readlines()
+
+        if "dtc" in self.knowledge_file:
+            self.parse_dtc_expert_template(lines)
+        elif "component" in self.knowledge_file:
+            self.parse_component_expert_template(lines)
+        elif "subsystem" in self.knowledge_file:
+            self.parse_subsystem_expert_template(lines)
+
         # only keep the actual data + headlines (categories)
         lines = [line for line in lines if line.count("#") in [0, 3] and line != "\n"]
 
@@ -37,21 +55,22 @@ class ExpertKnowledgeParser:
             if cat_cnt == 1:
                 self.dtc = line
             elif cat_cnt == 2:
-                self.fault_condition = line
-            elif cat_cnt == 3:
-                self.symptoms.append(line)
-            elif cat_cnt == 4:
-                self.suspect_components.append(line)
-            elif cat_cnt == 5:
                 self.occurs_with.append(line)
+            elif cat_cnt == 3:
+                self.fault_condition = line
+            elif cat_cnt == 4:
+                self.symptoms.append(line)
+            elif cat_cnt == 5:
+                self.suspect_components.append(line)
 
     def __str__(self) -> str:
+        # TODO: print all fields that are present (have values)
         return "DTC: " + self.dtc + "\nFault Condition: " + self.fault_condition + "\nSymptoms: " \
                + str(self.symptoms) + "\nSuspect Components: " + str(self.suspect_components) + "\nOccurs With: " \
                + str(self.occurs_with)
 
 
 if __name__ == '__main__':
-    knowledge_parser = ExpertKnowledgeParser("templates/expert_knowledge_template.txt")
+    knowledge_parser = ExpertKnowledgeParser("templates/dtc_expert_template.txt")
     knowledge_parser.parse_knowledge()
     print(knowledge_parser)
