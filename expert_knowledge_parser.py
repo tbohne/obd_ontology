a963c4c9a876b8a751a9f806f52d80a41a8b4793
tbohne <tim.bohne@dfki.de>
2022-10-04 11:20:13 +0200

    remove obsoltete OOP structure from knowledge parser

--- expert_knowledge_parser.py
+++ expert_knowledge_parser.py
@@ -2,132 +2,124 @@
 # -*- coding: utf-8 -*-
 # @author Tim Bohne
 
-from dtc_knowledge import DTCKnowledge
+from typing import Union
+
 from component_knowledge import ComponentKnowledge
+from dtc_knowledge import DTCKnowledge
 from subsystem_knowledge import SubsystemKnowledge
 
 
-class ExpertKnowledgeParser:
+def parse_dtc_expert_template(lines: list) -> DTCKnowledge:
+    """
+    Parses the DTC expert template.
+
+    :param lines: lines of the knowledge template
+    :return: parsed DTC knowledge
+    """
+    dtc = fault_condition = ""
+    occurs_with = []
+    symptoms = []
+    suspect_components = []
+
+    cat_cnt = 0
+    for line in lines:
+        if "###" in line:
+            cat_cnt += 1  # next category
+            continue
+
+        # should be data entry
+        assert line[0] == "-"
+        line = line[2:].strip()
+
+        if cat_cnt == 1:
+            dtc = line
+        elif cat_cnt == 2:
+            occurs_with.append(line)
+        elif cat_cnt == 3:
+            fault_condition = line
+        elif cat_cnt == 4:
+            symptoms.append(line)
+        elif cat_cnt == 5:
+            suspect_components.append(line)
+
+    return DTCKnowledge(dtc, occurs_with, fault_condition, symptoms, suspect_components)
+
+
+def parse_component_expert_template(lines: list) -> list:
+    """
+    Parses the suspect component expert template.
+
+    :param lines: lines of the knowledge template
+    :return: parsed component knowledge
+    """
+    suspect_components = []
+    # skip description etc.
+    lines = [line for line in lines if line[0] != "\n" and line[0] != "#"]
+
+    for line in lines:
+        # should be data entry
+        assert "-" in line[0:5]
+        if line[0] == "-":  # new component
+            suspect_component, oscilloscope = line[2:].strip().split(" ")
+            oscilloscope = True if "ja" in oscilloscope.lower() else False
+        else:  # complete entry
+            affected_by = [other_comp.strip() for other_comp in line[6:].split(",")]
+            suspect_components.append(ComponentKnowledge(suspect_component.strip(), oscilloscope, affected_by))
+    return suspect_components
+
+
+def parse_subsystem_expert_template(lines: list) -> SubsystemKnowledge:
+    """
+    Parses the vehicle subsystem expert template.
+
+    :param lines: lines of the knowledge template
+    :return: parsed subsystem knowledge
+    """
+    subsystem = verified_by = ""
+    contains = []
+
+    title_cnt = 0
+    for line in lines:
+        if "###" in line:
+            title_cnt += 1  # next category
+            continue
+        line = line[2:].strip()
+
+        if title_cnt == 1:  # subsystem name
+            subsystem = line
+        elif title_cnt == 4:  # 'contains' entries
+            contains.append(line)
+        elif title_cnt == 5:  # 'verified_by' entry
+            verified_by = line
+
+    return SubsystemKnowledge(subsystem, contains, verified_by)
+
+
+def parse_knowledge(knowledge_file: str) -> Union[DTCKnowledge, list, SubsystemKnowledge, None]:
     """
     Parses OBD-related expert knowledge from template file (cf. `templates/dtc_expert_template.txt`,
     `templates/component_expert_template`, `templates/subsystem_expert_template.txt`).
+
+    :param knowledge_file: expert knowledge file to be parsed
     """
+    with open(knowledge_file) as f:
+        lines = f.readlines()
 
-    def __init__(self, ) -> None:
-        pass
+    # only keep the actual data + headlines (categories)
+    lines = [line for line in lines if line.count("#") in [0, 3] and line != "\n"]
 
-    @staticmethod
-    def parse_dtc_expert_template(lines: list) -> DTCKnowledge:
-        """
-        Parses the DTC expert template.
-
-        :param lines: lines of the knowledge template
-        :return: parsed DTC knowledge
-        """
-        dtc = ""
-        occurs_with = []
-        fault_condition = ""
-        symptoms = []
-        suspect_components = []
-
-        cat_cnt = 0
-        for line in lines:
-            if "###" in line:
-                cat_cnt += 1  # next category
-                continue
-
-            # should be data entry
-            assert line[0] == "-"
-            line = line[2:].strip()
-
-            if cat_cnt == 1:
-                dtc = line
-            elif cat_cnt == 2:
-                occurs_with.append(line)
-            elif cat_cnt == 3:
-                fault_condition = line
-            elif cat_cnt == 4:
-                symptoms.append(line)
-            elif cat_cnt == 5:
-                suspect_components.append(line)
-
-        return DTCKnowledge(dtc, occurs_with, fault_condition, symptoms, suspect_components)
-
-    @staticmethod
-    def parse_component_expert_template(lines: list) -> list:
-        """
-        Parses the suspect component expert template.
-
-        :param lines: lines of the knowledge template
-        :return: parsed component knowledge
-        """
-        suspect_components = []
-        # skip description etc.
-        lines = [line for line in lines if line[0] != "\n" and line[0] != "#"]
-
-        for line in lines:
-            # should be data entry
-            assert "-" in line[0:5]
-            if line[0] == "-":  # new component
-                suspect_component, oscilloscope = line[2:].strip().split(" ")
-                oscilloscope = True if "ja" in oscilloscope.lower() else False
-            else:  # complete entry
-                affected_by = [other_comp.strip() for other_comp in line[6:].split(",")]
-                suspect_components.append(ComponentKnowledge(suspect_component.strip(), oscilloscope, affected_by))
-        return suspect_components
-
-    @staticmethod
-    def parse_subsystem_expert_template(lines: list) -> SubsystemKnowledge:
-        """
-        Parses the vehicle subsystem expert template.
-
-        :param lines: lines of the knowledge template
-        :return: parsed subsystem knowledge
-        """
-        subsystem = verified_by = ""
-        contains = []
-
-        title_cnt = 0
-        for line in lines:
-            if "###" in line:
-                title_cnt += 1  # next category
-                continue
-            line = line[2:].strip()
-
-            if title_cnt == 1:  # subsystem name
-                subsystem = line
-            elif title_cnt == 4:  # 'contains' entries
-                contains.append(line)
-            elif title_cnt == 5:  # 'verified_by' entry
-                verified_by = line
-
-        return SubsystemKnowledge(subsystem, contains, verified_by)
-
-    def parse_knowledge(self, knowledge_file: str) -> None:
-        """
-        Parses the OBD-related expert knowledge.
-
-        :param knowledge_file: expert knowledge file to be parsed
-        """
-        with open(knowledge_file) as f:
-            lines = f.readlines()
-
-        # only keep the actual data + headlines (categories)
-        lines = [line for line in lines if line.count("#") in [0, 3] and line != "\n"]
-
-        if "dtc" in knowledge_file:
-            return self.parse_dtc_expert_template(lines)
-        elif "component" in knowledge_file:
-            return self.parse_component_expert_template(lines)
-        elif "subsystem" in knowledge_file:
-            return self.parse_subsystem_expert_template(lines)
+    if "dtc" in knowledge_file:
+        return parse_dtc_expert_template(lines)
+    elif "component" in knowledge_file:
+        return parse_component_expert_template(lines)
+    elif "subsystem" in knowledge_file:
+        return parse_subsystem_expert_template(lines)
 
 
 if __name__ == '__main__':
-    knowledge_parser = ExpertKnowledgeParser()
-    print(knowledge_parser.parse_knowledge("templates/dtc_expert_template.txt"))
+    print(parse_knowledge("templates/dtc_expert_template.txt"))
     print("----------------------------------------------------------------------------")
-    for sus in knowledge_parser.parse_knowledge("templates/component_expert_template.txt"):
+    for sus in parse_knowledge("templates/component_expert_template.txt"):
         print(sus)
     print("----------------------------------------------------------------------------")
-    print(knowledge_parser.parse_knowledge("templates/subsystem_expert_template.txt"))
+    print(parse_knowledge("templates/subsystem_expert_template.txt"))
