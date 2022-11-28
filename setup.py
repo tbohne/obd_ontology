d170a99146ef600872108398e8150f84d6ba45e5
tbohne <tim.bohne@dfki.de>
2022-11-28 14:00:02 +0100

    add setup / requirements

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+__version__ = '0.0.1'
+URL = 'https://github.com/tbohne/OBDOntology'
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='OBDOntology',
+    version=__version__,
+    description='Ontology for capturing knowledge about on-board diagnosis (DTCs).',
+    author='Tim Bohne',
+    author_email='tim.bohne@dfki.de',
+    url=URL,
+    download_url=f'{URL}/archive/{__version__}.tar.gz',
+    keywords=[
+        'ontology',
+        'knowledge-graph',
+        'knowledge-base'
+    ],
+    python_requires='>=3.7',
+    install_requires=required,
+    packages=find_packages(),
+    include_package_data=True,
+)
