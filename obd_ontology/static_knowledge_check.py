27255238e2e110e3fae0fe574327f7eaa0cd349d
tbohne <tim.bohne@dfki.de>
2023-01-16 15:30:14 +0100

    implement static check for suspect component info (osci, affected_by, verifies, part_of)

--- obd_ontology/static_knowledge_check.py
+++ obd_ontology/static_knowledge_check.py
@@ -10,15 +10,19 @@ if __name__ == '__main__':
     dtc_instances = qt.query_all_dtc_instances(False)
     for dtc in dtc_instances:
         print(dtc)
-        print("\toccurs with:", qt.query_co_occurring_trouble_codes(dtc, False))
-        print("\tcategory:", qt.query_fault_cat_by_dtc(dtc, False))
-        print("\tcondition:", qt.query_fault_condition_by_dtc(dtc, False))
-        print("\tsymptoms:", qt.query_symptoms_by_dtc(dtc, False))
-        print("\tordered suspect components:")
+        print("\t- occurs with:", qt.query_co_occurring_trouble_codes(dtc, False))
+        print("\t- category:", qt.query_fault_cat_by_dtc(dtc, False))
+        print("\t- condition:", qt.query_fault_condition_by_dtc(dtc, False))
+        print("\t- symptoms:", qt.query_symptoms_by_dtc(dtc, False))
+        print("\t- ordered suspect components:")
         suspect_components = qt.query_suspect_component_by_dtc(dtc, False)
         ordered_sus_comp = {int(qt.query_diagnostic_association_by_dtc_and_sus_comp(dtc, comp, False)[0]): comp for comp
                             in suspect_components}
+
         for i in range(len(suspect_components)):
             print("\t\t-", ordered_sus_comp[i])
-
+            print("\t\t\tuse oscilloscope:", qt.query_oscilloscope_usage_by_suspect_component(ordered_sus_comp[i], False))
+            print("\t\t\taffected by:", qt.query_affected_by_relations_by_suspect_component(ordered_sus_comp[i], False))
+            print("\t\t\tverifies:", qt.query_verifies_relation_by_suspect_component(ordered_sus_comp[i], False))
+            print("\t\t\tpart of:", qt.query_contains_relation_by_suspect_component(ordered_sus_comp[i], False))
         print()
