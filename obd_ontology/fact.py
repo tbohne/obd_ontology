aab8aace046affa009ca977604bad0f12a784750
tbohne <tim.bohne@dfki.de>
2023-09-04 17:23:08 +0200

    refine semantic fact rep

--- obd_ontology/fact.py
+++ obd_ontology/fact.py
@@ -7,12 +7,23 @@ from typing import Tuple
 
 class Fact:
     """
-    Representation of a general fact to be entered into a triple store (knowledge graph).
+    Representation of a semantic fact to be entered into a triple store (knowledge graph).
     """
 
-    def __init__(self, triple: Tuple, property_fact=False) -> None:
+    def __init__(self, triple: Tuple, property_fact: bool = False) -> None:
+        """
+        Initializes the semantic fact.
+
+        :param triple: triple comprising the semantic fact <subject, predicate, object>
+        :param property_fact: whether it's a property fact
+        """
         self.triple = triple
         self.property_fact = property_fact
 
     def __str__(self) -> str:
+        """
+        Returns a string representation of the semantic fact.
+
+        :return: string representation of semantic fact
+        """
         return "triple: " + str(self.triple)
