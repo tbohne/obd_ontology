f3270936b3a8f04f0d9036a8529ed08e1d8a09d4
tbohne <tim.bohne@dfki.de>
2023-09-05 14:48:22 +0200

    implement bash script for automated Fuseki backups

---
+++ backup_kg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FUSEKI_URL="http://127.0.0.1:3030"
+DATASET_NAME="OBD"
+BACKUP_DIR="knowledge_base/live_kg_backups"
+BACKUP_FILE="$BACKUP_DIR/backup_$(date +\%Y\%m\%d_\%H\%M\%S).ttl"
+
+# construct URL for backup request
+BACKUP_URL="$FUSEKI_URL/$DATASET_NAME/data?graph=default"
+
+# trigger backup using curl
+curl -X GET "$BACKUP_URL" > $BACKUP_FILE
+
+# check HTTP response code to ensure the backup was successful (200 OK)
+HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$BACKUP_URL")
+
+echo "HTTP status: $HTTP_STATUS"
+
+# check if file was successfully created
+if [ -s "$BACKUP_FILE" ]; then
+  echo "backup completed successfully"
+else
+  echo "backup failed"
+fi
