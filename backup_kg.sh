b14acce302d9fda14ec891f970a42f5ab241ef4d
tbohne <tim.bohne@dfki.de>
2023-09-05 15:20:38 +0200

    refine KG backup script -- serialize as n-triples

--- backup_kg.sh
+++ backup_kg.sh
@@ -3,13 +3,13 @@
 FUSEKI_URL="http://127.0.0.1:3030"
 DATASET_NAME="OBD"
 BACKUP_DIR="knowledge_base/live_kg_backups"
-BACKUP_FILE="$BACKUP_DIR/backup_$(date +\%Y\%m\%d_\%H\%M\%S).ttl"
+BACKUP_FILE="$BACKUP_DIR/backup_$(date +\%Y\%m\%d_\%H\%M\%S).nt"
 
 # construct URL for backup request
 BACKUP_URL="$FUSEKI_URL/$DATASET_NAME/data?graph=default"
 
 # trigger backup using curl
-curl -X GET "$BACKUP_URL" > $BACKUP_FILE
+curl -H "Accept: application/n-triples" "$BACKUP_URL" > $BACKUP_FILE
 
 # check HTTP response code to ensure the backup was successful (200 OK)
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$BACKUP_URL")
