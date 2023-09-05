f770d00881f7cba53fa3e9fddf80907531f1331a
tbohne <tim.bohne@dfki.de>
2023-09-05 11:53:20 +0200

    refine knowledge snapshot

--- obd_ontology/knowledge_snapshot.py
+++ obd_ontology/knowledge_snapshot.py
@@ -9,15 +9,14 @@ from termcolor import colored
 from knowledge_graph_query_tool import KnowledgeGraphQueryTool
 
 
-def knowledge_snapshot_dtc_perspective():
+def knowledge_snapshot_dtc_perspective() -> None:
     """
     Presents a snapshot of the knowledge currently stored in the KG from a DTC-centric perspective.
     """
     print("###########################################################################")
     print("KNOWLEDGE SNAPSHOT - DTC PERSPECTIVE")
     print("###########################################################################\n")
-    dtc_instances = qt.query_all_dtc_instances(False)
-    for dtc in dtc_instances:
+    for dtc in qt.query_all_dtc_instances(False):
         print(colored(dtc, "yellow", "on_grey", ["bold"]))
         print(colored("\t- occurs with:", "blue", "on_grey", ["bold"]),
               qt.query_co_occurring_trouble_codes(dtc, False))
@@ -29,12 +28,10 @@ def knowledge_snapshot_dtc_perspective():
         vehicle_occurrences = qt.query_vehicle_by_dtc(dtc, False)
         for vehicle_occ in vehicle_occurrences:
             print("\t\t-", vehicle_occ)
-
         print(colored("\t- symptoms:", "blue", "on_grey", ["bold"]), qt.query_symptoms_by_dtc(dtc, False))
 
         sub_name = qt.query_indicates_by_dtc(dtc, False)
         sub_name = "" if len(sub_name) == 0 else sub_name[0]
-
         print(colored("\t- indicates subsystem:", "blue", "on_grey", ["bold"]), sub_name)
         print(colored("\t- indicates vehicle part(s):", "blue", "on_grey", ["bold"]),
               qt.query_vehicle_part_by_subsystem(sub_name, False))
@@ -43,7 +40,6 @@ def knowledge_snapshot_dtc_perspective():
         suspect_components = qt.query_suspect_components_by_dtc(dtc, False)
         ordered_sus_comp = {int(qt.query_priority_id_by_dtc_and_sus_comp(dtc, comp, False)[0]): comp for comp
                             in suspect_components}
-
         for i in range(len(suspect_components)):
             print(colored("\t\t- " + ordered_sus_comp[i], "yellow", "on_grey", ["bold"]))
             print(colored("\t\t\tuse oscilloscope:", "blue", "on_grey", ["bold"]),
@@ -58,15 +54,14 @@ def knowledge_snapshot_dtc_perspective():
     print("\n----------------------------------------------------------------------\n")
 
 
-def knowledge_snapshot_subsystem_perspective():
+def knowledge_snapshot_subsystem_perspective() -> None:
     """
     Presents a snapshot of the knowledge currently stored in the KG from a vehicle-subsystem-centric perspective.
     """
     print("###########################################################################")
     print("KNOWLEDGE SNAPSHOT - SUBSYSTEM PERSPECTIVE")
     print("###########################################################################\n")
-    subsystem_instances = qt.query_all_vehicle_subsystem_instances(False)
-    for subsystem in subsystem_instances:
+    for subsystem in qt.query_all_vehicle_subsystem_instances(False):
         print(colored(subsystem, "yellow", "on_grey", ["bold"]))
         print(colored("\t- contains:", "blue", "on_grey", ["bold"]),
               qt.query_contains_relation_by_subsystem(subsystem, False))
@@ -75,15 +70,14 @@ def knowledge_snapshot_subsystem_perspective():
     print("\n----------------------------------------------------------------------\n")
 
 
-def knowledge_snapshot_component_set_perspective():
+def knowledge_snapshot_component_set_perspective() -> None:
     """
     Presents a snapshot of the knowledge currently stored in the KG from a component-set-centric perspective.
     """
     print("###########################################################################")
     print("KNOWLEDGE SNAPSHOT - COMPONENT SET PERSPECTIVE")
     print("###########################################################################\n")
-    component_set_instances = qt.query_all_component_set_instances(False)
-    for comp_set in component_set_instances:
+    for comp_set in qt.query_all_component_set_instances(False):
         print(colored(comp_set, "yellow", "on_grey", ["bold"]))
         print(colored("\t- verified by:", "blue", "on_grey", ["bold"]),
               qt.query_verifies_relations_by_component_set(comp_set, False))
@@ -91,15 +85,14 @@ def knowledge_snapshot_component_set_perspective():
               qt.query_includes_relation_by_component_set(comp_set, False))
 
 
-def knowledge_snapshot_component_perspective():
+def knowledge_snapshot_component_perspective() -> None:
     """
     Presents a snapshot of the knowledge currently stored in the KG from a component-centric perspective.
     """
     print("###########################################################################")
     print("KNOWLEDGE SNAPSHOT - COMPONENT PERSPECTIVE")
     print("###########################################################################\n")
-    component_instances = qt.query_all_component_instances(False)
-    for comp in component_instances:
+    for comp in qt.query_all_component_instances(False):
         print(colored(comp, "yellow", "on_grey", ["bold"]))
         print(colored("\t- oscilloscope:", "blue", "on_grey", ["bold"]),
               qt.query_oscilloscope_usage_by_suspect_component(comp, False)[0])
@@ -110,15 +103,14 @@ def knowledge_snapshot_component_perspective():
     print("\n----------------------------------------------------------------------\n")
 
 
-def knowledge_snapshot_parallel_osci_perspective():
+def knowledge_snapshot_parallel_osci_perspective() -> None:
     """
     Presents a snapshot of the knowledge currently stored in the KG regarding parallel oscillogram sets.
     """
     print("###########################################################################")
     print("KNOWLEDGE SNAPSHOT - PARALLEL OSCILLOGRAM SET PERSPECTIVE")
     print("###########################################################################\n")
-    osci_set_instances = qt.query_all_parallel_rec_oscillogram_set_instances(False)
-    for osci_set_id in osci_set_instances:
+    for osci_set_id in qt.query_all_parallel_rec_oscillogram_set_instances(False):
         osci_set_id = osci_set_id.split("#")[1]
         print(colored(osci_set_id, "yellow", "on_grey", ["bold"]))
         oscillogram_instances_by_set = qt.query_oscillograms_by_parallel_osci_set(osci_set_id, False)
@@ -126,30 +118,28 @@ def knowledge_snapshot_parallel_osci_perspective():
             print(colored("\t- oscillogram instance:", "blue", "on_grey", ["bold"]), osci.split("#")[1])
 
 
-def knowledge_snapshot_oscillogram_perspective():
+def knowledge_snapshot_oscillogram_perspective() -> None:
     """
     Presents a snapshot of the knowledge currently stored in the KG regarding oscillograms.
     """
     print("###########################################################################")
     print("KNOWLEDGE SNAPSHOT - OSCILLOGRAM PERSPECTIVE")
     print("###########################################################################\n")
-    osci_instances = qt.query_all_recorded_oscillograms(False)
-    for osci in osci_instances:
+    for osci in qt.query_all_recorded_oscillograms(False):
         osci_id = osci.split("#")[1]
         print(colored("osci: " + osci.split("#")[1], "yellow", "on_grey", ["bold"]))
         time_series = qt.query_time_series_by_oscillogram_instance(osci_id, False)[0]
         print(colored("\t- time series excerpt:", "blue", "on_grey", ["bold"]), time_series[:50], "...")
 
 
-def knowledge_snapshot_oscillogram_classification_perspective():
+def knowledge_snapshot_oscillogram_classification_perspective() -> None:
     """
     Presents a snapshot of the knowledge currently stored in the KG regarding oscillogram classifications.
     """
     print("###########################################################################")
     print("KNOWLEDGE SNAPSHOT - OSCILLOGRAM CLASSIFICATION PERSPECTIVE")
     print("###########################################################################\n")
-    osci_classification_instances = qt.query_all_oscillogram_classifications(False)
-    for osci_classification in osci_classification_instances:
+    for osci_classification in qt.query_all_oscillogram_classifications(False):
         osci_classification_id = osci_classification.split("#")[1]
         print(colored(osci_classification_id, "yellow", "on_grey", ["bold"]))
         print(colored("\t- model id:", "blue", "on_grey", ["bold"]),
@@ -183,15 +173,14 @@ def knowledge_snapshot_oscillogram_classification_perspective():
         print(colored("\t- prediction:", "blue", "on_grey", ["bold"]), prediction[0] if len(prediction) > 0 else "")
 
 
-def knowledge_snapshot_manual_inspection_perspective():
+def knowledge_snapshot_manual_inspection_perspective() -> None:
     """
     Presents a snapshot of the knowledge currently stored in the KG regarding manual inspections.
     """
     print("###########################################################################")
     print("KNOWLEDGE SNAPSHOT - MANUAL INSPECTION PERSPECTIVE")
     print("###########################################################################\n")
-    manual_inspection_instances = qt.query_all_manual_inspection_instances(False)
-    for manual_inspection in manual_inspection_instances:
+    for manual_inspection in qt.query_all_manual_inspection_instances(False):
         manual_inspection_id = manual_inspection.split("#")[1]
         print(colored(manual_inspection_id, "yellow", "on_grey", ["bold"]))
         suspect_comp_instance = qt.query_suspect_component_by_classification(manual_inspection_id, False)
@@ -207,15 +196,14 @@ def knowledge_snapshot_manual_inspection_perspective():
         print(colored("\t- prediction:", "blue", "on_grey", ["bold"]), prediction[0] if len(prediction) > 0 else "")
 
 
-def knowledge_snapshot_diag_log_perspective():
+def knowledge_snapshot_diag_log_perspective() -> None:
     """
     Presents a snapshot of the knowledge currently stored in the KG regarding diagnosis logs.
     """
     print("###########################################################################")
     print("KNOWLEDGE SNAPSHOT - DIAGNOSIS LOG PERSPECTIVE")
     print("###########################################################################\n")
-    diag_log_instances = qt.query_all_diag_log_instances(False)
-    for diag_log in diag_log_instances:
+    for diag_log in qt.query_all_diag_log_instances(False):
         diag_log_id = diag_log.split("#")[1]
         print(colored(diag_log_id, "yellow", "on_grey", ["bold"]))
         print(colored("\t- date:", "blue", "on_grey", ["bold"]), qt.query_date_by_diag_log(diag_log_id, False)[0])
@@ -241,15 +229,14 @@ def knowledge_snapshot_diag_log_perspective():
             print("\t\t-", diag_step.split("#")[1])
 
 
-def knowledge_snapshot_fault_path_perspective():
+def knowledge_snapshot_fault_path_perspective() -> None:
     """
     Presents a snapshot of the knowledge currently stored in the KG regarding fault paths.
     """
     print("###########################################################################")
     print("KNOWLEDGE SNAPSHOT - FAULT PATH PERSPECTIVE")
     print("###########################################################################\n")
-    fault_path_instances = qt.query_all_fault_path_instances(False)
-    for fault_path in fault_path_instances:
+    for fault_path in qt.query_all_fault_path_instances(False):
         fault_path_id = fault_path.split("#")[1]
         fault_path_desc = qt.query_fault_path_description_by_id(fault_path_id, False)
         print(colored("fault path: " + fault_path_id, "yellow", "on_grey", ["bold"]))
@@ -261,15 +248,14 @@ def knowledge_snapshot_fault_path_perspective():
             print("\t\t-", fault_condition_desc)
 
 
-def knowledge_snapshot_vehicle_perspective():
+def knowledge_snapshot_vehicle_perspective() -> None:
     """
     Presents a snapshot of the knowledge currently stored in the KG regarding vehicles.
     """
     print("###########################################################################")
     print("KNOWLEDGE SNAPSHOT - VEHICLE PERSPECTIVE")
     print("###########################################################################\n")
-    vehicle_instances = qt.query_all_vehicle_instances()
-    for vehicle_id, hsn, tsn, vin, model in vehicle_instances:
+    for vehicle_id, hsn, tsn, vin, model in qt.query_all_vehicle_instances():
         vehicle_id = vehicle_id.split("#")[1]
         print(colored(vehicle_id, "yellow", "on_grey", ["bold"]))
         print(colored("\t- HSN: " + hsn, "blue", "on_grey", ["bold"]))
