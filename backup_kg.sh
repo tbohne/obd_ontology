8589f93b5bbc53e24bd7c42276e4bf217c998ac2
tbohne <tim.bohne@dfki.de>
2023-09-06 09:52:19 +0200

    KG backup script -- KG URL + dataset name as command line args

--- backup_kg.sh
+++ backup_kg.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-FUSEKI_URL="http://127.0.0.1:3030"
-DATASET_NAME="OBD"
+# Check if at least two command-line arguments are provided
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 FUSEKI_URL DATASET_NAME"
+  exit 1
+fi
+
+FUSEKI_URL="$1"
+DATASET_NAME="$2"
 BACKUP_DIR="knowledge_base/live_kg_backups"
 BACKUP_FILE="$BACKUP_DIR/backup_$(date +\%Y\%m\%d_\%H\%M\%S).nt"
 KG_SNAPSHOT_FILE="$BACKUP_DIR/kg_snapshot_$(date +\%Y\%m\%d_\%H\%M\%S).txt"
@@ -10,7 +16,7 @@ KG_SNAPSHOT_FILE="$BACKUP_DIR/kg_snapshot_$(date +\%Y\%m\%d_\%H\%M\%S).txt"
 BACKUP_URL="$FUSEKI_URL/$DATASET_NAME/data?graph=default"
 
 # trigger backup using curl
-curl -H "Accept: application/n-triples" "$BACKUP_URL" > $BACKUP_FILE
+curl -H "Accept: application/n-triples" "$BACKUP_URL" > "$BACKUP_FILE"
 
 # check HTTP response code to ensure the backup was successful (200 OK)
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$BACKUP_URL")
@@ -25,5 +31,5 @@ else
 fi
 
 echo "creating KG snapshot.."
-python obd_ontology/knowledge_snapshot.py --perspective expert >> $KG_SNAPSHOT_FILE
-python obd_ontology/knowledge_snapshot.py --perspective diag >> $KG_SNAPSHOT_FILE
+python obd_ontology/knowledge_snapshot.py --perspective expert >> "$KG_SNAPSHOT_FILE"
+python obd_ontology/knowledge_snapshot.py --perspective diag >> "$KG_SNAPSHOT_FILE"
