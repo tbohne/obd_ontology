cfb306ac50a04b1ae9d7773890a5c75d9801bccb
tbohne <tim.bohne@dfki.de>
2023-10-06 17:06:52 +0200

    extend clear_diag_knowledge script -- removal of oscillograms

--- obd_ontology/clear_diag_knowledge.py
+++ obd_ontology/clear_diag_knowledge.py
@@ -28,6 +28,26 @@ def clear_heatmap_facts(exp_enhancer: ExpertKnowledgeEnhancer, kg_query_tool: Kn
     exp_enhancer.fuseki_connection.remove_outdated_facts_from_knowledge_graph(facts_to_be_removed)
 
 
+def clear_oscillogram_facts(exp_enhancer: ExpertKnowledgeEnhancer, kg_query_tool: KnowledgeGraphQueryTool) -> None:
+    """
+    Clears the oscillogram facts (recorded oscillograms) currently stored in the KG.
+
+    :param exp_enhancer: instance of the expert knowledge enhancer
+    :param kg_query_tool: instance of the KG query tool
+    """
+    facts_to_be_removed = []
+    osci_classifications = kg_query_tool.query_all_oscillogram_classifications(False)
+    for classification in osci_classifications:
+        classification_uuid = classification.split("#")[1]
+        osci = kg_query_tool.query_oscillogram_by_classification_instance(classification_uuid, False)[0]
+        osci_uuid = osci.split("#")[1]
+        time_series = kg_query_tool.query_time_series_by_oscillogram_instance(osci_uuid, False)[0]
+        facts_to_be_removed.append(exp_enhancer.generate_time_series_fact(osci_uuid, time_series, True))
+        facts_to_be_removed.append(exp_enhancer.generate_classifies_fact(classification_uuid, osci_uuid, False))
+        facts_to_be_removed.append(exp_enhancer.generate_oscillogram_fact(osci_uuid, False))
+    exp_enhancer.fuseki_connection.remove_outdated_facts_from_knowledge_graph(facts_to_be_removed)
+
+
 if __name__ == '__main__':
     """
     This script is used to reduce the size of the currently hosted KG by removing the largest aspects of the diagnostic
@@ -38,3 +58,4 @@ if __name__ == '__main__':
     expert_knowledge_enhancer = ExpertKnowledgeEnhancer()
     knowledge_graph_query_tool = KnowledgeGraphQueryTool()
     clear_heatmap_facts(expert_knowledge_enhancer, knowledge_graph_query_tool)
+    clear_oscillogram_facts(expert_knowledge_enhancer, knowledge_graph_query_tool)
