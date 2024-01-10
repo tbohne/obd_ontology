74a3b734c20f930976c44dcc9df27cde0fe0460c
tbohne <tim.bohne@dfki.de>
2024-01-10 16:27:16 +0100

    compress KG backups -- solves #51

--- backup_kg.sh
+++ backup_kg.sh
@@ -9,14 +9,14 @@ fi
 FUSEKI_URL="$1"
 DATASET_NAME="$2"
 BACKUP_DIR="knowledge_base/live_kg_backups"
-BACKUP_FILE="$BACKUP_DIR/backup_$(date +\%Y_\%m_\%d-\%H_\%M_\%S).nt"
+BACKUP_FILE="$BACKUP_DIR/backup_$(date +\%Y_\%m_\%d-\%H_\%M_\%S).nt.gz"
 KG_SNAPSHOT_FILE="$BACKUP_DIR/kg_snapshot_$(date +\%Y_\%m_\%d-\%H_\%M_\%S).txt"
 
 # construct URL for backup request
 BACKUP_URL="$FUSEKI_URL/$DATASET_NAME/data?graph=default"
 
-# trigger backup using curl
-curl -H "Accept: application/n-triples" "$BACKUP_URL" > "$BACKUP_FILE"
+# trigger backup using curl and compress the result using gzip
+curl -H "Accept: application/n-triples" "$BACKUP_URL" | gzip > "$BACKUP_FILE"
 
 # check HTTP response code to ensure the backup was successful (200 OK)
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$BACKUP_URL")
