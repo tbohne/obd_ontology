db582d0f03beb4bd181f9f1ff7ea604e99460a6a
tbohne <tim.bohne@dfki.de>
2024-05-24 15:14:58 +0200

    extend model rep

--- obd_ontology/component_knowledge.py
+++ obd_ontology/component_knowledge.py
@@ -10,7 +10,8 @@ class ComponentKnowledge:
     Representation of vehicle-component-related expert knowledge.
     """
 
-    def __init__(self, suspect_component: str, oscilloscope: bool, affected_by: List[str]) -> None:
+    def __init__(self, suspect_component: str, oscilloscope: bool, affected_by: List[str],
+                 associated_chan: str = "", chan_of_interest: List[str] = []) -> None:
         """
         Initializes the component knowledge.
 
@@ -18,10 +19,14 @@ class ComponentKnowledge:
         :param oscilloscope: whether oscilloscope measurement possible / reasonable
         :param affected_by: list of components whose misbehavior could affect the correct functioning of the component
                             under consideration
+        :param associated_chan: channel associated with the component, i.e., 'hasChannel' relation
+        :param chan_of_interest: proposed channels of interest, i.e., 'hasCOI' relation
         """
         self.suspect_component = suspect_component
         self.oscilloscope = oscilloscope
         self.affected_by = affected_by
+        self.associated_chan = associated_chan
+        self.chan_of_interest = chan_of_interest
 
     def __str__(self) -> str:
         """
