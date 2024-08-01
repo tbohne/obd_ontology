0a107092504b9c174ac04b69f9405bcf719c2603
tbohne <tim.bohne@dfki.de>
2024-08-01 13:35:18 +0200

    adapt component knowledge -- channel semantics

--- obd_ontology/component_knowledge.py
+++ obd_ontology/component_knowledge.py
@@ -11,15 +11,18 @@ class ComponentKnowledge:
     """
 
     def __init__(self, suspect_component: str, oscilloscope: bool, affected_by: List[str],
-                 associated_chan: str = "", chan_of_interest: List[str] = []) -> None:
+                 associated_chan: List[str] = [], chan_of_interest: List[str] = []) -> None:
         """
         Initializes the component knowledge.
+
+        Associated channels can also be part of COI. The associated channels are only further information for
+        knowing where (at which component) to measure. The COI are the ones that are used for classification.
 
         :param suspect_component: component to be checked
         :param oscilloscope: whether oscilloscope measurement possible / reasonable
         :param affected_by: list of components whose misbehavior could affect the correct functioning of the component
                             under consideration
-        :param associated_chan: channel associated with the component, i.e., 'hasChannel' relation
+        :param associated_chan: channels associated with the component, i.e., 'hasChannel' relation
         :param chan_of_interest: proposed channels of interest, i.e., 'hasCOI' relation
         """
         self.suspect_component = suspect_component
@@ -34,5 +37,10 @@ class ComponentKnowledge:
 
         :return: string representation of component knowledge
         """
-        return "Suspect Component: " + self.suspect_component + "\nUse Oscilloscope: " + str(self.oscilloscope) \
-            + "\nAffected By: " + str(self.affected_by)
+        return (
+                "Suspect Component: " + self.suspect_component
+                + "\nUse Oscilloscope: " + str(self.oscilloscope)
+                + "\nAffected By: " + str(self.affected_by)
+                + "\nAssociated Channels: " + str(self.associated_chan)
+                + "\nCOI: " + str(self.chan_of_interest)
+        )
